f61b54fc23a584af54aac5b494e5e38ba2222964
batikan <emiruar123@gmail.com>
2022-08-19 20:44:58 +0300

    first commit

---
+++ Day 1/app.js
@@ -0,0 +1,12 @@
+//console.log('param1','param2','param3');
+
+
+/* asddasdsasadoıkhasbdoasadskads*/
+
+
+console.log("Hello World!");
+console.log('Hello World!');
+console.log(`Hello World!`);
+
+
+console.log(3**2);
