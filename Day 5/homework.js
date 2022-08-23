90a3c13ed742eb4c07ce6744c35147bb9f36a32a
batikan <emiruar123@gmail.com>
2022-08-23 20:57:10 +0300

    Day-5 arrays part-1

---
+++ Day 5/homework.js
@@ -0,0 +1,70 @@
+//LEVEL 1
+
+//Step-1
+const myArr = [1, 3, 5, "Emir", [1, 2, 3], "Javascript", 10];
+
+//Step-3
+//console.log(`My Arry Length : ${myArr.length}`);
+
+//Step-4
+//console.log(myArr[0]);
+//console.log(myArr[Math.floor(myArr.length / 2)]);
+//console.log(myArr[myArr.length - 1]);
+
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+
+//Step-9
+/*
+console.log(itCompanies[0]);
+console.log(itCompanies[Math.floor(itCompanies.length/2)]);
+console.log(itCompanies[itCompanies.length-1]);
+*/
+
+//Step-11
+//console.log(itCompanies);
+//console.log((itCompanies.join(' ').toUpperCase().split(' ')));
+
+
+//Step-12
+//console.log(`${itCompanies.join(' ')} are bit IT companies.`);
+
+//Step-13
+/*
+var result=itCompanies.indexOf('Oracle');
+console.log(result);
+result !=-1 ? console.log(`${itCompanies[result]}`) : console.log('Not Found!');
+*/
+
+
+//Step-14
+//console.log('yapamadım');
+
+
+//step-15
+//console.log(itCompanies.sort());
+
+//step-16
+//console.log(itCompanies.reverse());
+
+//Step-17
+//console.log(itCompanies.reverse());
+//let result=itCompanies.slice(0,3);
+//let result2=itCompanies.reverse().slice(0,3);
+//let result3=itCompanies.slice(Math.floor(itCompanies.length/2),6);
+//console.log(result);
+//console.log(result2);
+//console.log(result3);
+//console.log(itCompanies.shift());
+console.log(itCompanies);
+console.log(itCompanies[Math.floor(itCompanies.length/2)].);
+console.log(itCompanies);
+//itCompanies.pop()
+//console.log(itCompanies.reverse());
